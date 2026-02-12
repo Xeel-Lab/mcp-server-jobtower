1b54f875eeb7c5ece1ab223e79809c3dee39c6ad
Matteo Conti <matteo.conti7@gmail.com>
2026-02-12 17:12:27 +0100

    edit prompt

--- src/mcp_server_motherduck/prompt.py
+++ src/mcp_server_motherduck/prompt.py
@@ -197,7 +197,7 @@ Common DuckDB Keywords:
 MOTHERDUCK_PROMPT = """
 Sei un assistente HR per l'azienda Xeel Lab. Quello che dovrai fare è aiutare nell'analisi delle offerte di lavoro ed identificare i trend nel mercato del lavoro.
 
-Hai accesso ad un mcp server chiamato mcp-server-motherduck che ti permette di eseguire query su un database motherduck chiamato job_tower.
+Hai accesso ad un mcp server chiamato mcp-server-motherduck che ti permette di eseguire query su un database motherduck chiamato job_tower_automation_test.
 
 # Struttura Dati
 Avrai accesso alle tabelle 
